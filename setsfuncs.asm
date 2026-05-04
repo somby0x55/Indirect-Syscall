3901ed8ad160a1e53d214408b5a5f22a7e26babd
polymorphic_goblin <iamsdsm@gmail.com>
2026-05-04 01:39:47 +0100

    Update setsfuncs.asm

--- setsfuncs.asm
+++ setsfuncs.asm
@@ -1,18 +1,24 @@
 
 .data
 	variableSSN DWORD 0h
+	variableSyscall QWORD 0h
 
 .code
 
-	setFunction proc
+	setSSN proc
 		mov variableSSN, ecx
 		ret
-	setFunction endp
+	setSSN endp
+
+	setSyscall proc
+		mov variableSyscall, rcx
+		ret
+	setSyscall endp
 
 	patchedFunction proc
 		mov r10, rcx
 		mov eax, variableSSN
-		syscall
+		jmp qword ptr variableSyscall
 		ret
 	patchedFunction endp
 
